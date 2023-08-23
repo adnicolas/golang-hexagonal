0a1c511c4f3f1dd06e1d07961404d80bc854b652
anp9648@gmail.com <anp9648@gmail.com>
2023-08-23 16:47:44 +0200

    use gin web framework

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 const httpAddr = ":8081"
 
 func main() {
 	fmt.Println("Server running on", httpAddr)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/health", healthHandler)
-	log.Fatal(http.ListenAndServe(httpAddr, mux))
+	srv := gin.New()
+	srv.GET("/health", healthHandler)
+	log.Fatal(srv.Run(httpAddr))
 }
 
-// Underscore convention to indicate that I'm not going to use that parameter
-func healthHandler(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("everything is ok!"))
+func healthHandler(ctx *gin.Context) {
+	ctx.String(http.StatusOK, "everything is ok!")
 }
