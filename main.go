5f7f0cfc3b171d171034142c788ab0edfee447e9
anp9648@gmail.com <anp9648@gmail.com>
2023-08-23 16:34:53 +0200

    add first endpoint (server health check)

--- main.go
+++ main.go
@@ -11,5 +11,11 @@ const httpAddr = ":8081"
 func main() {
 	fmt.Println("Server running on", httpAddr)
 	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
 	log.Fatal(http.ListenAndServe(httpAddr, mux))
+}
+
+func healthHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("everything is ok!"))
 }
