6a83ce40b3b988bb28a75d818e76c2fbf0ccfe6b
anp9648@gmail.com <anp9648@gmail.com>
2023-08-27 11:23:05 +0200

    add envconfig library to consume config from env variables

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -13,24 +13,19 @@ import (
 	"github.com/adnicolas/golang-hexagonal/internal/platform/bus/inmemory"
 	"github.com/adnicolas/golang-hexagonal/internal/platform/server"
 	pg "github.com/adnicolas/golang-hexagonal/internal/platform/storage/postgresql"
+	"github.com/kelseyhightower/envconfig"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host            = "localhost"
-	port            = 8081
-	shutdownTimeout = 10 * time.Second
-
-	dbUser     = "postgres"
-	dbPassword = "dockerized_metadata"
-	dbHost     = "localhost"
-	dbPort     = 5433
-	dbName     = "metadata"
-	dbTimeout  = 5 * time.Second
-)
-
 func Run() error {
-	postgresURI := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	// envconfig library allows the use of environment variables for configuration
+	var cfg config
+	err := envconfig.Process("gohex", &cfg)
+	if err != nil {
+		return err
+	}
+
+	postgresURI := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.DbHost, cfg.DbPort, cfg.DbUser, cfg.DbPassword, cfg.DbName)
 	db, err := sql.Open("postgres", postgresURI)
 	if err != nil {
 		return err
@@ -42,7 +37,7 @@ func Run() error {
 		eventBus   = inmemory.NewEventBus()
 	)
 
-	userRepository := pg.NewUserRepository(db, dbTimeout)
+	userRepository := pg.NewUserRepository(db, cfg.DbTimeout)
 
 	creatingUserService := creating.NewUserService(userRepository, eventBus)
 	fetchingUserService := fetching.NewUserService(userRepository)
@@ -57,7 +52,21 @@ func Run() error {
 	eventBus.Subscribe(usuario.UserCreatedEventType, creating.NewIncreaseUsersCounterOnUserCreated(increasingUserService))
 
 	// Server initialization with empty context
-	ctx, srv := server.New(context.Background(), host, port, shutdownTimeout, commandBus, queryBus)
+	ctx, srv := server.New(context.Background(), cfg.Host, cfg.Port, cfg.ShutdownTimeout, commandBus, queryBus)
 
 	return srv.Run(ctx)
+}
+
+type config struct {
+	// Server configuration
+	Host            string        `default:"localhost"`
+	Port            uint          `default:"8081"`
+	ShutdownTimeout time.Duration `default:"10s"`
+	// Database configuration
+	DbUser     string        `default:"postgres"`
+	DbPassword string        `default:"dockerized_metadata"`
+	DbHost     string        `default:"localhost"`
+	DbPort     uint          `default:"5433"`
+	DbName     string        `default:"metadata"`
+	DbTimeout  time.Duration `default:"5s"`
 }
